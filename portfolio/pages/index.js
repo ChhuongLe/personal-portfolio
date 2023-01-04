47151aaf566dab5d749d4c223e16699f2a0a1ab4
Chhuong Le <chhuongle@gmail.com>
2023-01-04 15:58:16 -0800

    initialize nextJS and tailwindCSS

---
+++ portfolio/pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Chhuong's Portfolio</title>
+      </Head>
+      <h1 className="p-10 text-red-500">Hello from the index component</h1>
+
+      {/* Header */}
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
+    </>
+  )
+}
