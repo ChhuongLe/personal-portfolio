7a78ee56d576fcdb48af357585173a229322c80a
Chhuong Le <chhuongle@gmail.com>
2023-01-24 15:34:50 -0800

    implement hamburger menu for mobile

--- portfolio/components/Header.jsx
+++ portfolio/components/Header.jsx
@@ -1,8 +1,9 @@
 import { React, useState } from 'react'
 import Link from 'next/link'
 import { SocialIcon } from 'react-social-icons'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import { AiOutlineMenu } from 'react-icons/ai'
+import { CiCircleRemove } from 'react-icons/ci'
 
 export default function Header () {
   const [isClick, setClick] = useState(false);
@@ -14,14 +15,31 @@ export default function Header () {
   }
   if(isClick === true) {
     dropDownMenu = (
-      <div className="absolute">
-        THIS IS THE DROPDOWN I GUESS
-      </div>
+          <motion.div
+            initial={{
+              x: 500,
+              opacity: 0,
+            }}
+            animate={{
+              x:0,
+              opacity: 1,
+            }}
+            transition={{ duration:0.3,}}
+            className="absolute bg-[#A27B5C]/90 h-[300px] w-[150px] right-0 top-2 z-20 p-2 rounded-sm cursor-default">
+            <button className="absolute right-2 top-5 text-2xl" onClick={handleClick}><CiCircleRemove size={25}/></button>
+            <div className="absolute flex flex-col space-y-5 top-[50px] left-[25px]">
+              <Link href="#hero" className="navElement">Main Page</Link>
+              <Link href="#about" className="navElement">About</Link>
+              <Link href="#experiece" className="navElement">Experience</Link>
+              <Link href="#skills" className="navElement">Skills</Link>
+              <Link href="#projects" className="navElement" >Projects</Link>
+            </div>
+          </motion.div>
     )
   }
 
   return (
-    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-10 xl:items-center">
       <motion.div
       initial={{
         x: -500,
--- portfolio/components/Hero.jsx
+++ portfolio/components/Hero.jsx
@@ -22,7 +22,7 @@ export default function Hero() {
         width={500}
         height={500}
         className="relative rounded-full h-32 w-32 mx-auto object-cover"/>
-      <div className="z-20">
+      <div className="z-10">
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[8px] sm:tracking-[15px]">Software Engineer</h2>
         <h1 className="text-md sm:text-2xl lg:text-6xl font-semibold px-10">
           <span className="mr-3">{text}</span>
