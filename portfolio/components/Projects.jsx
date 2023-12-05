7d3e24c732307d838039fd3b57447ce27b73030e
Chhuong Le <chhuongle@gmail.com>
2023-12-04 18:21:32 -0800

    fully deployed

--- portfolio/components/Projects.jsx
+++ portfolio/components/Projects.jsx
@@ -18,7 +18,7 @@ export default function Projects({ projects }) {
       }}
       className="flex relative h-screen overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Projects</h3>
-      <div className="relative mt-16 sm:mt-0 w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-thin scrollbar-track-gray-400 scrollbar-thumb-[#DCD7C9]" >
+      <div className="relative mt-16 sm:mt-0 w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400 scrollbar-thumb-[#DCD7C9]" >
         {projects.map((project, i)=>{
           return (
             <div key={i} className= "w-screen flex flex-shrink-0 snap-center flex-col space-y-5 items-center justify-center p-20 md:p-44">
